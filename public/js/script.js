d789f97b223366533c9f04a9f423ffa396396702
Adarsh Shaw <dev.adarsh279@gmail.com>
2025-09-21 20:44:25 +0530

    Chess Render

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,43 @@
 const socket = io();
+const chess = new Chess()
+const boardElement = document.querySelector(".chessboard")
+
+let draggedPiece = null;
+let sourceSquare = null;
+let playerRole = null;
+
+const renderBoard = () => {
+    const board = chess.board()
+    boardElement.innerHTML = ""
+    board.forEach((row, rowindex) => {
+        row.forEach((square, squareindex) => {
+            const squareElement = document.createElement("div")
+            squareElement.classList.add("square",
+                (rowindex + squareindex) % 2 === 0 ? "light" : "dark"
+            )
+
+            squareElement.dataset.row = rowindex
+            squareElement.dataset.col = squareindex
+
+            if (square) {
+                const pieceElement = document.createElement("div")
+                pieceElement.classList.add("piece", square.color === 'W' ? 'white' : 'black')
+
+                pieceElement.innerText = "";
+                pieceElement.draggable = playerRole === square.color
+
+                pieceElement.addEventListener("dragstart", () => {
+                    if (pieceElement.draggable) {
+                        draggedPiece = pieceElement
+                        sourceSquare = {row: rowindex, col: squareindex}
+                    }
+                })
+            }
+
+        })
+    });
+}
+
+const handleMove = () => {}
+
+const getPieceUnicode = () => {}
